1c99be6fa9815bd74466bcedff009ae66f8f7cb3
zhangshiwei <zhangshiwei199204@163.com>
2019-10-30 01:03:21 +0800

    first commit push test

---
+++ lib/pages/tabs/Tabs.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_wei/pages/cart/Cart.dart';
+import 'package:flutter_wei/pages/category/Category.dart';
+import 'package:flutter_wei/pages/home/Home.dart';
+import 'package:flutter_wei/pages/user/User.dart';
+
+class Tabs extends StatefulWidget {
+  Tabs({Key key}) : super(key: key);
+
+  _TabsState createState() => _TabsState();
+}
+
+class _TabsState extends State<Tabs> {
+  int _currentIndex = 0;
+
+  List _pageList = [HomePage(), CategoryPage(), CartPage(), UserPage()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("jdshop"),
+      ),
+      body: this._pageList[this._currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: this._currentIndex,
+        onTap: (index) {
+          setState(() {
+            this._currentIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        fixedColor: Colors.red,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text("首页"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            title: Text("分类"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            title: Text("购物车"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            title: Text("我的"),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/routers/router.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_wei/pages/search/Search.dart';
+
+import '../pages/tabs/Tabs.dart';
+
+// 配置路由
+final routes = {
+  '/': (context) => Tabs(),
+  '/search': (context) => SearchPage(),
+};
+
+var onGenerateRoute = (RouteSettings settings) {
+// 统一处理
+  final String name = settings.name;
+  final Function pageContentBuilder = routes[name];
+  if (pageContentBuilder != null) {
+    if (settings.arguments != null) {
+      final Route route = MaterialPageRoute(
+          builder: (context) =>
+              pageContentBuilder(context, arguments: settings.arguments));
+      return route;
+    } else {
+      final Route route =
+          MaterialPageRoute(builder: (context) => pageContentBuilder(context));
+      return route;
+    }
+  }
+};
